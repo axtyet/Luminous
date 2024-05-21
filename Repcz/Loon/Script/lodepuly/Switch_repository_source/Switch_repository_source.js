d4d24fb7fae636a58d80367f28f5054edb5baf70
GitHub Action <action@github.com>
2024-05-21 14:44:10 +0800

    2024-05-21 14:44

--- Repcz/Loon/Script/lodepuly/Switch_repository_source/Switch_repository_source.js
+++ Repcz/Loon/Script/lodepuly/Switch_repository_source/Switch_repository_source.js
@@ -14,30 +14,30 @@ var headers = $request.headers
 delete headers.host
 delete headers.Host
 
-if (changeTo == "Bitbucket") {
+if (changeTo == "Bitbucket - 四网直连") {
     headers["host"] = "bitbucket.org"
-} else if (changeTo == "GitLab") {
+} else if (changeTo == "GitLab - 电信联通直连") {
     headers["host"] = "gitlab.com"
-} else if (changeTo == "GitHub") {
+} else if (changeTo == "GitHub - 访问不畅") {
     headers["host"] = "raw.githubusercontent.com"
 }
 
 if (url.startsWith(githubPrefix)) {
-    if (changeTo == "Bitbucket") {
+    if (changeTo == "Bitbucket - 四网直连") {
         url = url.replace(githubPrefix,gitbucketPrefix)
-    } else if (changeTo == "GitLab") {
+    } else if (changeTo == "GitLab - 电信联通直连") {
         url = url.replace(githubPrefix,gitlabPrefix)
     }
 } else if (url.startsWith(gitlabPrefix)) {
-    if (changeTo == "Bitbucket") {
+    if (changeTo == "Bitbucket - 四网直连") {
         url = url.replace(gitlabPrefix,gitbucketPrefix)
-    } else if (changeTo == "GitHub") {
+    } else if (changeTo == "GitHub - 访问不畅") {
         url = url.replace(gitlabPrefix,githubPrefix)
     }
 } else if (url.startsWith(gitbucketPrefix)) {
-    if (changeTo == "GitLab") {
+    if (changeTo == "GitLab - 电信联通直连") {
         url = url.replace(gitbucketPrefix,gitlabPrefix)
-    } else if (changeTo == "GitHub") {
+    } else if (changeTo == "GitHub - 访问不畅") {
         url = url.replace(gitbucketPrefix,githubPrefix)
     }
 }
