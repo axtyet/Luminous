a8770da60f21742f2d6185960a6569fe6f93ea99
GitHub Action <action@github.com>
2024-05-22 16:42:50 +0800

    2024-05-22 16:42

--- Yu9191/Rewrite/dhzs.js
+++ Yu9191/Rewrite/dhzs.js
@@ -1,13 +1,14 @@
 /*
 电话助手通杀 
 [rewrite_local]
-https://kkyun.com/api/(accounts|numbers|apps)/user url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/Yu9191/Rewrite/dhzs.js
+https://kkyun.com/api/(accounts|numbers|apps)/(user|start_task) url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/Yu9191/Rewrite/dhzs.js
 [mitm]
 hostname = kkyun.com
 
 */
 $done({
     body: $response.body.replace(/("vip_status"\s*:\s*)0/g, '$12')
-    .replace(/("is_vip"\s*:\s*)false/g, '$1true')
+		.replace(/("is_vip"\s*:\s*)false/g, '$1true')
     .replace(/("vip_name"\s*:\s*")[^"]*"/g, '$1永久VIP"')
+		.replace(/("status"\s*:\s*)0/g, '$11')		
 });
