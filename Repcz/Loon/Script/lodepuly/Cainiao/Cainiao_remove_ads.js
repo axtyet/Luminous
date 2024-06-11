461e862cd8f9db96ca2ab9e451ef6abebb1fff78
GitHub Action <action@github.com>
2024-06-12 02:43:37 +0800

    2024-06-12 02:43

--- Repcz/Loon/Script/lodepuly/Cainiao/Cainiao_remove_ads.js
+++ Repcz/Loon/Script/lodepuly/Cainiao/Cainiao_remove_ads.js
@@ -1,7 +1,7 @@
 /*
 脚本引用https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
 */
-// 2024-03-23 19:00
+// 2024-06-08 09:40
 
 const url = $request.url;
 if (!$response.body) $done({});
@@ -31,7 +31,8 @@ if (url.includes(".guoguo.nbnetflow.ads.mshow")) {
       "1428", // 幸运号
       "1524", // 抽现金
       "1525", // 幸运包裹
-      "1638" // 为你精选了一些商品
+      "1638", // 为你精选了一些商品
+      "1910" // 618促销红包
     ];
     for (let i of items) {
       if (obj.data?.[i]) {
