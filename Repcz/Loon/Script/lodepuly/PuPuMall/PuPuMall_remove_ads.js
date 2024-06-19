da4873725d718f374acd0f256a9964c306ab6b70
GitHub Action <action@github.com>
2024-06-19 16:43:38 +0800

    2024-06-19 16:43

--- Repcz/Loon/Script/lodepuly/PuPuMall/PuPuMall_remove_ads.js
+++ Repcz/Loon/Script/lodepuly/PuPuMall/PuPuMall_remove_ads.js
@@ -1,6 +1,7 @@
 /*
 脚本引用 https://raw.githubusercontent.com/kelv1n1n/script/main/js/pp.js
 */
+
 let url = $request.url;
 let body = $response.body;
 let obj = JSON.parse(body);
@@ -10,38 +11,41 @@ const mine = "position_types=60%2C560%2C850%2C860%2C890%2C2400";
 const search_hot = "/search/hot_keywords";
 const recommend = "/resource_preload/list_h5_resource";
 const recommendPro = "/recommendation/interests/products";
+const adv = "/advertisement/v1";
+
 if (url.indexOf(mine) != -1) {
   obj.data = [];
   body = JSON.stringify(obj);
-  console.log("清除啦");
   $done({body});
 }
 
 if (url.indexOf(search) != -1) {
   obj.data = [];
   body = JSON.stringify(obj);
-  // console.log("热搜清除啦");
   $done({body});
 }
 
 if (url.indexOf(search_hot) != -1) {
   obj.data = [];
   body = JSON.stringify(obj);
-  // console.log("热搜发现清除啦");
   $done({body});
 }
 
 if (url.indexOf(recommend) != -1) {
   obj.data = obj.data.filter(item => item.filename !== "RecommendProduct.29e31893.js");
   body = JSON.stringify(obj);
-  // console.log("推荐清除啦");
   $done({body});
 }
 
 if (url.indexOf(recommendPro) != -1) {
   obj.data = {};
   body = JSON.stringify(obj);
-  // console.log("推荐商品清除啦");
+  $done({body});
+}
+
+if (url.indexOf(adv) != -1) {
+  obj.data = obj.data.filter(item => ![50,90].includes(item.region_code)); 
+  body = JSON.stringify(obj);
   $done({body});
 }
 
