9c9dd6251ff523b274865c2eb663fc92cc92cd71
GitHub Action <action@github.com>
2024-06-26 14:44:37 +0800

    2024-06-26 14:44

---
+++ Guding88/Script/DreamFace.js
@@ -0,0 +1,20 @@
+/*
+
+DreamFace：https://apps.apple.com/app/id1624172324
+
+[rewrite_local]
+^https?:\/\/dreamfaceapp\.com\/df-server\/user\/save_user_login url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/Guding88/Script/DreamFace.js
+
+[MITM]
+hostname = dreamfaceapp.com
+
+*/
+var guding = JSON.parse($response.body);
+guding.data.rights= {
+      "renewal": true,
+      "vip_label": true,
+      "expires_date": 148204937166000,
+      "expires_date_format": "6666-06-06 06:06:06.666",
+      "vip_product_id": "96"
+    };
+$done({ body: JSON.stringify(guding) });
