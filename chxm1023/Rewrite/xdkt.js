230eae23bbbb10eb40bcc73005207cd33a6dbc60
GitHub Action <action@github.com>
2024-07-02 19:41:26 +0800

    2024-07-02 19:41

--- chxm1023/Rewrite/xdkt.js
+++ chxm1023/Rewrite/xdkt.js
@@ -2,7 +2,7 @@
 
 项目名称：小滴课堂
 下载地址：https://t.cn/A6T1XX5Z
-更新日期：2024-04-256
+更新日期：2024-07-03
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -10,7 +10,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api-v2\.xdclass\.net\/api\/account\/v\d\/detail url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/chxm1023/Rewrite/xdkt.js
+
+^https:\/\/api-v2\.xdclass\.net\/api\/account\/(v\d\/detail|v1\/login) url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/chxm1023/Rewrite/xdkt.js
 
 [mitm]
 hostname = api-v2.xdclass.net
