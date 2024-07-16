99aa203fbb33640232078048ccc0a86e5c35349d
GitHub Action <action@github.com>
2024-07-16 21:41:11 +0800

    2024-07-16 21:41

--- Yu9191/Rewrite/MGTV.js
+++ Yu9191/Rewrite/MGTV.js
@@ -9,6 +9,11 @@ https://raw.githubusercontent.com/RuCu6/QuanX/main/Rewrites/Cube/cnftp.snippet
 https://mobile-stream.api.mgtv.com/v1/video/source? url script-request-header https://raw.githubusercontent.com/axtyet/Luminous/main/Yu9191/Rewrite/MGTV.js
 https://nuc.api.mgtv.com/GetUserInfo url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/Yu9191/Rewrite/mgtv.js
 https://mobile-stream.api.mgtv.com/v1/video/source url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/Yu9191/Rewrite/mgtv.js
+#港区
+^https://mobile.api.mgtv.com/v8/video/getSource url script-request-header https://raw.githubusercontent.com/axtyet/Luminous/main/Yu9191/Rewrite/MGTV.js
+#播放页开通提示移除
+http://vip.bz.mgtv.com/client/dynamic_entry url reject
+
 [mitm] 
 hostname = *.mgtv.com
 
