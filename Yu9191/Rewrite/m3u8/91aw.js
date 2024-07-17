5a301031b9152addcdae3c4fdaa7d8ebd50893bc
GitHub Action <action@github.com>
2024-07-17 19:41:49 +0800

    2024-07-17 19:41

--- Yu9191/Rewrite/m3u8/91aw.js
+++ Yu9191/Rewrite/m3u8/91aw.js
@@ -6,5 +6,5 @@
 https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
 
 [mitm]
-hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net
+hostname = *.cloudfront.net, *.ninghaixin.club, *.wbwxe.com, wbapi.wbwxe.com, d13dw8kzjnavm.cloudfront.net, hxnd.*.vip
 */
