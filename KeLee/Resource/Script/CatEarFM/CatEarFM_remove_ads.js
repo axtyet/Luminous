9494defd4d453881b7b291b34b984d77ef0c5424
GitHub Action <action@github.com>
2024-08-12 23:43:13 +0800

    2024-08-12 23:43

--- KeLee/Resource/Script/CatEarFM/CatEarFM_remove_ads.js
+++ KeLee/Resource/Script/CatEarFM/CatEarFM_remove_ads.js
@@ -1,25 +1,11 @@
-// 2024-08-12 22:31:22
+// 2024-08-12 22:41:30
 const url = $request.url;
-if (!$response.body) $done({});
-
-let obj = JSON.parse($response.body);
-
-function filterIcons(obj) {
+if (url.includes("/site/icons")) {
+    let obj = JSON.parse($response.body);
     if (obj.info && obj.info.icons && Array.isArray(obj.info.icons)) {
         obj.info.icons = obj.info.icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
     }
+    $done({ body: JSON.stringify(obj) });
+} else {
+    $done({});
 }
-
-function filterInfo(obj) {
-    const titlesToFilter = ["直播间", "广播剧", "免流服务"];
-    if (obj.info && obj.info.title) {
-        obj.info.title = obj.info.title.filter(title => !titlesToFilter.includes(title));
-    }
-}
-
-if (url.includes("/site/icons")) {
-    filterIcons(obj);
-    filterInfo(obj);
-}
-
-$done({ body: JSON.stringify(obj) });
