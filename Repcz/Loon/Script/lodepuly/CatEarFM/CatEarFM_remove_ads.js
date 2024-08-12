40c212c4cbeb633e12f3f11e7a0d36974f7bf80d
GitHub Action <action@github.com>
2024-08-12 22:44:03 +0800

    2024-08-12 22:44

--- Repcz/Loon/Script/lodepuly/CatEarFM/CatEarFM_remove_ads.js
+++ Repcz/Loon/Script/lodepuly/CatEarFM/CatEarFM_remove_ads.js
@@ -1,11 +1,25 @@
-// 2024-08-12 20:33:39
+// 2024-08-12 22:31:22
 const url = $request.url;
-if (url.includes("/site/icons")) {
-    let obj = JSON.parse($response.body);
+if (!$response.body) $done({});
+
+let obj = JSON.parse($response.body);
+
+function filterIcons(obj) {
     if (obj.info && obj.info.icons && Array.isArray(obj.info.icons)) {
         obj.info.icons = obj.info.icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
     }
-    $done({ body: JSON.stringify(obj) });
-} else {
-    $done({});
 }
+
+function filterInfo(obj) {
+    const titlesToFilter = ["直播间", "广播剧", "免流服务"];
+    if (obj.info && obj.info.title) {
+        obj.info.title = obj.info.title.filter(title => !titlesToFilter.includes(title));
+    }
+}
+
+if (url.includes("/site/icons")) {
+    filterIcons(obj);
+    filterInfo(obj);
+}
+
+$done({ body: JSON.stringify(obj) });
