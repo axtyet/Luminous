335134d347a6a422e01d0c6874b9a9401d09f664
GitHub Action <action@github.com>
2024-08-16 17:42:04 +0800

    2024-08-16 17:42

--- ddgksf2013/MOYU/WindyProCrack.js
+++ ddgksf2013/MOYU/WindyProCrack.js
@@ -6,7 +6,7 @@ tg频道：https://t.me/ddgksf2021
 
 [rewrite_local]
 
-^https?:\/\/account\.windy\.com\/api\/info url script-response-body https://github.com/ddgksf2013/MoYu/raw/main/WindyProCrack.js
+^https?:\/\/account\.windy\.com\/api\/info url script-response-body https://raw.githubusercontent.com/axtyet/Luminous/main/ddgksf2013/MOYU/WindyProCrack.js
 
 [mitm]
 
