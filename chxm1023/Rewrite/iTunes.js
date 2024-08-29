3f14ebdc3232e1d580513dd900024e9d73c0d7ca
GitHub Action <action@github.com>
2024-08-29 17:43:03 +0800

    2024-08-29 17:43

--- chxm1023/Rewrite/iTunes.js
+++ chxm1023/Rewrite/iTunes.js
@@ -29,6 +29,7 @@ const yearlyid = `${bundle_id}.yearly`;
 const yearlysubscription = `${bundle_id}.yearlysubscription`;
 
 const list = {
+  'Period': { cm: 'timeb', hx: 'hxpda', id: "com.hanchongzan.book.vip", latest: "chxm1023" }, //闪电记账
   'EarthSpirit': { cm: 'timeb', hx: 'hxpda', id: "Facey_Forever", latest: "chxm1023" }, //Facey-专业彩妆P图神器
   'Packet': { cm: 'timeb', hx: 'hxpda', id: "com.aaaalab.nepacket.iap.full", latest: "chxm1023" }, //HTTPS抓包
   'AllMyBatteries': { cm: 'timeb', hx: 'hxpda', id: "AllMyBatteries_Ultimate", latest: "chxm1023" }, //AllMyBatteries-电池管家
