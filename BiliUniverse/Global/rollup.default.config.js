6ce218967e880877bf2093f987318374b649043a
GitHub Action <action@github.com>
2024-09-24 13:45:30 +0800

    2024-09-24 13:45

--- BiliUniverse/Global/rollup.default.config.js
+++ BiliUniverse/Global/rollup.default.config.js
@@ -1,33 +1,33 @@
-import json from '@rollup/plugin-json';
+import json from "@rollup/plugin-json";
 import commonjs from "@rollup/plugin-commonjs";
-import terser from '@rollup/plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from "@rollup/plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default [
 	{
-		input: 'src/BiliBili.Global.request.js',
+		input: "src/BiliBili.Global.request.js",
 		output: {
-			file: 'js/BiliBili.Global.request.js',
-			format: 'es',
+			file: "js/BiliBili.Global.request.js",
+			format: "es",
 			banner: "/* README: https://github.com/BiliUniverse */\nconsole.log('📺 BiliBili: 🌐 Global Request')",
 		},
 		plugins: [json(), commonjs(), nodeResolve(), terser()]
 	},
 	{
-		input: 'src/BiliBili.Global.response.js',
+		input: "src/BiliBili.Global.response.js",
 		output: {
-			file: 'js/BiliBili.Global.response.js',
-			format: 'es',
+			file: "js/BiliBili.Global.response.js",
+			format: "es",
 			banner: "/* README: https://github.com/BiliUniverse */\nconsole.log('📺 BiliBili: 🌐 Global Response')",
 		},
 		plugins: [json(), commonjs(), nodeResolve(), terser()]
 	},
 	/*
 	{
-		input: 'src/BiliIntl.Global.request.js',
+		input: "src/BiliIntl.Global.request.js",
 		output: {
-			file: 'js/BiliIntl.Global.request.js',
-			format: 'es',
+			file: "js/BiliIntl.Global.request.js",
+			format: "es",
 		},
 		plugins: [json(), commonjs(), nodeResolve(), terser()]
 	},
