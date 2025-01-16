268bf407dc42041ea3e57a32c7f82ce68cb4c34a
GitHub Action <action@github.com>
2025-01-16 21:03:31 +0800

    2025-01-16 21:03

---
+++ ddgksf2013/MOYU/VisProCrack.js
@@ -0,0 +1,17 @@
+/***********************************
+> ScriptName        Vis Pro
+> Author            @ddgksf2013
+> TgChannel         https://t.me/ddgksf2021
+> ScriptURL         https://raw.githubusercontent.com/axtyet/Luminous/main/ddgksf2013/MOYU/VisProCrack.js
+
+[rewrite_local]
+
+# ～ RevenueCat@ddgksf2013
+^https:\/\/rc\.visionarytech\.ltd\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
+^https:\/\/rc\.visionarytech\.ltd\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/ddgksf2013/Scripts/master/deleteHeader.js
+
+[mitm]
+
+hostname=rc.visionarytech.ltd
+
+***********************************/
