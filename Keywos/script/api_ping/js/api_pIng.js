00305e42532698fedfdf5e31fa13ed6876ce0638
GitHub Action <action@github.com>
2025-02-10 05:43:44 +0800

    2025-02-10 05:43

--- Keywos/script/api_ping/js/api_pIng.js
+++ Keywos/script/api_ping/js/api_pIng.js
@@ -1,5 +1,6 @@
-// 2025-02-08 18:33:32
+// 2025-02-10 05:29:33
 !(async () => {
+  let timeouts = 5000;
   try {
     let url = typeof $request !== "undefined" && $request.url,
       ms,
@@ -9,10 +10,30 @@
           .map((i) => i.split("="))
           .map(([k, v]) => [k, decodeURIComponent(v)])
       );
+    timeouts = Number(ins.timeout) || 5000;
     if (ins.url == "test") {
-      ms = '1';
+      ms = "1";
     } else {
-      ms = await Get(ins.url);
+      ms = await new Promise((resolve, reject) => {
+        let e = Date.now();
+        const timeoutPromise = new Promise((_, reject) => {
+          setTimeout(() => {
+            reject("");
+            resolve(timeouts);
+          }, timeouts);
+        });
+        const reqPromise = new Promise((resolve) => {
+          $httpClient.get(ins.url, resolve);
+        });
+        Promise.race([reqPromise, timeoutPromise])
+          .then((i) => {
+            resolve(Date.now() - e);
+          })
+          .catch((error) => {
+            reject(error);
+            resolve(timeouts);
+          });
+      });
     }
     $done({
       response: {
@@ -24,6 +45,7 @@
         body: JSON.stringify({
           sp: 1,
           ms: ms,
+          timeouts: timeouts,
         }),
       },
     });
@@ -36,25 +58,3 @@
     console.log(e.message);
   })
   .finally(() => $done());
-async function Get(url) {
-  return new Promise((resolve, reject) => {
-    let e = Date.now();
-    const timeoutPromise = new Promise((_, reject) => {
-      setTimeout(() => {
-        reject("");
-        resolve("5000");
-      }, 5000);
-    });
-    const reqPromise = new Promise((resolve) => {
-      $httpClient.get(url, resolve);
-    });
-    Promise.race([reqPromise, timeoutPromise])
-      .then((i) => {
-        resolve(Date.now() - e);
-      })
-      .catch((error) => {
-        reject(error);
-        resolve("6666");
-      });
-  });
-}
