09c6c67a1642387eecd03fb63e677d4d6897c17b
GitHub Action <action@github.com>
2025-02-12 23:58:33 +0800

    2025-02-12 23:58

--- Keywos/script/api_ping/js/api_ping.js
+++ Keywos/script/api_ping/js/api_ping.js
@@ -1,14 +1,10 @@
+// 2025-02-12 23:38:27
 !(async () => {
   let timeouts = 5000;
   try {
     let url = typeof $request !== "undefined" && $request.url,
-      ms,
-      ins = Object.fromEntries(
-        (url.split("?")[1] || "")
-          .split("&")
-          .map((i) => i.split("="))
-          .map(([k, v]) => [k, decodeURIComponent(v)])
-      );
+      ms = "",
+      ins;
     const getEnv = () => {
       return "undefined" != typeof Egern
         ? "Egern"
@@ -26,54 +22,62 @@
         ? "Shadowrocket"
         : void 0;
     };
-    timeouts = Number(ins.timeout) || 5000;
-    if (ins.url == "test") {
-      ms = "1";
-    } else {
-      ms = await new Promise((resolve, reject) => {
-        let e = Date.now();
-        const timeoutPromise = new Promise((_, reject) => {
-          setTimeout(() => {
-            reject("");
-            resolve(timeouts);
-          }, timeouts);
+    if (url) {
+      ins = Object.fromEntries(
+        (url.split("?")[1] || "")
+          .split("&")
+          .map((i) => i.split("="))
+          .map(([k, v]) => [k, decodeURIComponent(v)])
+      );
+      timeouts = Number(ins.timeout) || 5000;
+      if (ins.url == "test") {
+        ms = "1";
+      } else {
+        ms = await new Promise((resolve, reject) => {
+          let e = Date.now();
+          const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+              reject("");
+              resolve(timeouts);
+            }, timeouts);
+          });
+          let reqPromise;
+          if (getEnv() == "Quantumult X") {
+            reqPromise = new Promise((resolve, reject) => {
+              $task
+                .fetch({
+                  url: ins.url,
+                  method: "GET",
+                })
+                .then((response) => {
+                  resolve(response);
+                })
+                .catch((error) => {
+                  reject(error);
+                  resolve(timeouts);
+                });
+            });
+          } else {
+            reqPromise = new Promise((resolve) => {
+              $httpClient.get(ins.url, resolve);
+            });
+          }
+          Promise.race([reqPromise, timeoutPromise])
+            .then(() => {
+              resolve(Date.now() - e);
+            })
+            .catch((error) => {
+              reject(error);
+              resolve(timeouts);
+            });
         });
-        let reqPromise;
-        if (getEnv() == "Quantumult X") {
-          reqPromise = new Promise((resolve, reject) => {
-            $task
-              .fetch({
-                url: ins.url,
-                method: "GET",
-              })
-              .then((response) => {
-                resolve(response);
-              })
-              .catch((error) => {
-                reject(error);
-              });
-          });
-        } else {
-          reqPromise = new Promise((resolve) => {
-            $httpClient.get(ins.url, resolve);
-          });
-        }
-        Promise.race([reqPromise, timeoutPromise])
-          .then(() => {
-            resolve(Date.now() - e);
-          })
-          .catch((error) => {
-            reject(error);
-            resolve(timeouts);
-          });
-      });
+      }
     }
     const headers = {
       "Content-Type": "application/json; charset=utf-8",
       "Access-Control-Allow-Origin": "*",
     };
     if (getEnv() == "Quantumult X") {
-      const s = $environment.version.split(" ");
       $done({
         status: "HTTP/1.1 200 OK",
         headers: headers,
