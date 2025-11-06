5ae8aaacc7b0e6a9c8fe07e270ef6ac5971c1546
GitHub Action <action@github.com>
2025-11-06 22:50:01 +0800

    2025-11-06 22:50

--- wool_scripts/Scripts/xiaohongshu/xiaohongshu.js
+++ wool_scripts/Scripts/xiaohongshu/xiaohongshu.js
@@ -67,6 +67,7 @@ if (url.includes("/system_service/splash_config")) {
 }
 
 if (url.includes("/note/imagefeed?") || url.includes("/note/feed?")) {
+  console.log('打印原body：' + JSON.stringify(obj));
   // 信息流 图片
   if (obj?.data?.length > 0) {
     if (obj.data[0]?.note_list?.length > 0) {
@@ -105,7 +106,7 @@ if (url.includes("/note/imagefeed?") || url.includes("/note/feed?")) {
 if (url.includes("/note/live_photo/save")) {
   console.log('原body：' + rsp_body);
   const rsp = $.getdata("fmz200.xiaohongshu.feed.rsp");
-  console.log("读取缓存key：fmz200.xiaohongshu.feed.rsp");
+  console.log("读取缓存key[fmz200.xiaohongshu.feed.rsp]的值：" + rsp);
   // console.log("读取缓存val：" + rsp);
   if (rsp == null || rsp.length === 0) {
     console.log('缓存无内容，返回原body');
